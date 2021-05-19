23515e34f7a43ec3cc1bcc27e424d62988680bed
Rahman Samadzade <56510976+capwan@users.noreply.github.com>
2021-05-19 08:57:19 +0400

    updated main.py
    added "Hello @username","What's city is interesting for you?" when you write "/start" command to bot.

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ from pyowm.utils.config import get_default_config
 config_dict = get_default_config()
 config_dict['language'] = 'ru'
 owm = pyowm.OWM('c19ac12a384b37dd79f6408bf1560726', config_dict)
-bot = telebot.TeleBot("1763102450:AAG8R7etHjR14_7Gb_BfHIrs1l3ek2gnSbM", parse_mode=None)
+bot = telebot.TeleBot("1763102450:AAG8R7etHjR14_7Gb_BfHIrs1l3ek2gnSbM")
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    send_mess = f"<b>Привет {message.from_user.first_name}! </b>\nКакой город тебя интересует?"
+    bot.send_message(message.chat.id, send_mess, parse_mode='html')
+
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
@@ -19,5 +25,15 @@ def echo_all(message):
 
     bot.reply_to(message, weather_info)
 
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    mgr = owm.weather_manager()
+    observation = mgr.forecast_at_place(message.text, 'daily')
+    answer = forecast.will_be_clear_at(timestamps.tomorrow())
+    w = observation.weather
+    weather_info = "В городе "+message.text+" сейчас "+w.detailed_status+"\n"
+    weather_info += "Температура: " + str(w.temperature('celsius')['temp']) + "\n"
+    weather_info += "Скорость ветра: " + str(w.wind()['speed']) + " м/с" + "\n"
 
-bot.polling()
+
+bot.polling(none_stop=True)

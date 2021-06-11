d35103ce73f50212e4c891cc1c0c8eda10f96f98
Rahman Samadzade <56510976+capwan@users.noreply.github.com>
2021-06-11 14:58:01 +0400

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def echo_all(message):
          weather_info += "💨 Скорость ветра: " + str(w.wind()['speed']) + " м/с" + "\n"
          weather_info += "🌕 Восход солнца: " + str(w.sunrise_time(timeformat='iso')) + "\n"
          weather_info += "🌑 Заход солнца: " + str(w.sunset_time(timeformat='iso')) + "\n"
-         weather_info += "Влажность: " + str(w.humidity('dict')) + "\n"
+         weather_info += "Влажность: " + str(w.humidity()) + "\n"
          
 
 
